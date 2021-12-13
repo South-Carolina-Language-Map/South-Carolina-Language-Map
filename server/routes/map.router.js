251767e61f8fa85654a47b7fbd2a0a31e101bfc0
Julia Bayless <julia.allistair@gmail.com>
2021-12-13 14:16:45 -0600

    map router complete, not tested

--- server/routes/map.router.js
+++ server/routes/map.router.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 //GET all data for plotting map sites and hover over sites
 router.get('/', (req, res) => {
-  // GET route code here
-
+ 
+//query to get geolocation and language variety +id for map view
   let queryTextForMap = `
   SELECT * FROM "sites"
   JOIN "languages" ON "languages".id = "sites".languages_id
@@ -18,11 +18,30 @@ router.get('/', (req, res) => {
             console.log(error);
             res.sendStatus(500);
         })
-});
+}); //end GET for map sites and hover
 
 
 //get all for map language detail
-router.get
+router.get('/:id', (req, res) => {
+    
+    //query for details on a specific detail and all the data associated with it
+    let queryTextForLanguageDetails = `
+    SELECT * FROM "sites"
+    JOIN "regions" ON "regions".id = "sites".region_id
+    JOIN "languages" ON "languages".id = "sites".languages_id
+    JOIN "examples" ON "languages".id = "examples".languages_id
+    JOIN "catagories" ON "languages".category_id = "categories".id
+    `
+
+    pool.query(queryTextForLanguageDetails)
+    .then((result) => {
+        res.send(result.rows)
+    })
+    .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    })
+}); //end GET all for details
 
 
 
