ecc34a14fed64577ae3cb19394e3b775495d4bb1
Jack Barrett <jbarrett1347@gmail.com>
2021-12-13 11:40:54 -0600

    Added untested language routes. Post currently unfinished

--- server/routes/languages.router.js
+++ server/routes/languages.router.js
@@ -2,18 +2,100 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
+router.get('/languages', (req, res) => {
+  pool.query(`SELECT * FROM "languages";`)
+    .then(() => {
+      res.send(response.rows);
+      console.log('GET Languages success');
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+      console.log('ERROR in Languages GET');
+    })
 });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
+router.get(`/languages/${id}`, (req, res) => {
+  const languageID = req.params.id;
+  const queryText = `
+  SELECT FROM "languages"
+  WHERE "id" = $1
+  ;`;
+
+  pool.query(queryText, [languageID])
+    .then(() => {
+      res.send(response.rows);
+      console.log('GET Language by id success');
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+      console.log('ERROR in GET Language by id');
+    })
 });
+
+router.post('/languages', (req, res) => {
+  const newLanguage = req.body.language;
+  const queryText = `
+  INSERT INTO "languages" (language, glottocode, description, endonym, global_speakers, sc_speakers, category_id)
+  VALUES ($1, $2, $3, $4, $5, $6, $7)
+  ;`;
+
+  pool.query(queryText, [newLanguage.language, newLanguage.glottocode, newLanguage.description,
+  newLanguage.endonym, newLanguage.global_speakers, newLanguage.sc_speakers, newLanguage.category_id])
+    .then(() => {
+      res.sendStatus(201);
+      console.log('POST Language success');
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+      console.log('ERROR in Language POST');
+    })
+
+});
+
+router.put(`/languages/${id}`, (req, res) => {
+  const id = req.params.id;
+  const updatedLanguage = req.body.language;
+  const queryText = `
+  UPDATE "languages"
+  SET "language" = $2,
+  "glottocode" = $3,
+  "description" = $4,
+  "global_speakers" = $5,
+  "sc_speakers" = $6,
+  "category_id" = $7
+  WHERE "id" = $1
+  ;`;
+
+  pool.query(queryText, [id, updatedLanguage.language, updatedLanguage.glottocode, updatedLanguage.description,
+    updatedLanguage.endonym, updatedLanguage.global_speakers, updatedLanguage.sc_speakers, updatedLanguage.category_id])
+    .then(() => {
+      res.sendStatus(200);
+      console.log('PUT Language success');
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+      console.log('ERROR in Language PUT');
+    })
+});
+
+router.delete(`/languages/${id}`, (req, res) => {
+  const languageID = req.params.id;
+  const queryText = `
+  DELETE FROM "languages"
+  WHERE "id" = $1
+  ;`;
+
+  pool.query(queryText, [languageID])
+    .then(() => {
+      res.send(response.rows);
+      console.log('DELETE Language success');
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+      console.log('ERROR in DELETE Language');
+    })
+});
+
+
 
 module.exports = router;
