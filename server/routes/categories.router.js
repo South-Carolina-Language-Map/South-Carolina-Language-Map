8d8af711056637d0e4972760346879602571ce83
Jack Barrett <jbarrett1347@gmail.com>
2021-12-13 10:41:42 -0600

    Added untested catagories routingd

--- server/routes/categories.router.js
+++ server/routes/categories.router.js
@@ -2,18 +2,73 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
+router.get('/catagories', (req, res) => {
+  pool.query(`SELECT * FROM "catagories";`)
+    .then((response) => {
+      res.send(response.rows);
+      console.log('GET Catagories success');
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+      console.log('ERROR in Catagories GET', err);
+    })
 });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
+router.post('/catagories', (req, res) => {
+  const name = req.body.category;
+  const queryText = `
+  INSERT INTO "catagories" (name)
+  VALUES ($1);
+  `;
+
+  pool.query(queryText, [name])
+    .then(() => {
+      sendStatus(201);
+      console.log('POST Catagories success');
+    })
+    .catch((err) => {
+      sendStatus(500);
+      console.log('ERROR in Catagories POST', err);
+    });
 });
+
+router.put('/catagories/${id}', (req, res) => {
+  const id = req.params.id;
+  const name = req.body.name;
+  const queryText = `
+  UPDATE "category"
+  SET "name" = $1
+  WHERE "id" = $2
+  ;`
+
+  pool.query(queryText, [name, id])
+    .then(() => {
+      res.sendStatus(200);
+      console.log('PUT Catagories success');
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+      console.log('ERROR in category PUT', err);
+    })
+});
+
+router.delete('/categories/${id}', (req, res) => {
+  const categoryID = req.params.id;
+  const queryText = `
+  DELETE FROM "catagories" WHERE "id" = $1
+  ;`
+
+  pool.query(queryText, [categoryID])
+    .then(() => {
+      console.log('DELETE Category success')
+      res.sendStatus(200)
+    })
+    .catch((err) => {
+      console.log('ERROR in category DELETE', err);
+      res.sendStatus(500);
+    })
+});
+
+
 
 module.exports = router;
