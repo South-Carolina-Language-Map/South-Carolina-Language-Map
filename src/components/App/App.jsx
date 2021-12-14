fa0a8004ac9a99b4d9eba13581379143d9d341d3
Rob Cone <robcone86@gmail.com>
2021-12-14 11:24:33 -0600

    map update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -39,7 +39,7 @@ function App() {
             <AboutPage />
           </Route>
 
-          {/* For protected routes, the view could show one of several things on the same route.
+          {/* For protectesttestted routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
