b74cf2cab94be8b937e555ff6c16aa3ea0a52bd0
Kongxib Vue <kongcvue@gmail.com>
2021-12-15 13:21:34 -0600

    created admin reducers

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -23,8 +23,8 @@ const adminReducer = combineReducers({
 // Lets make a bigger object for our store, with the objects from our reducers.
 // This is what we get when we use 'state' inside of 'mapStateToProps'
 const rootReducer = combineReducers({
-  adminReducer, // contains registrationMessage and loginMessage
-  user, // will have an id and username if someone is logged in
+  adminReducer, 
+  
 });
 
 export default rootReducer;
