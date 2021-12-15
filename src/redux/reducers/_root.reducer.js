05c88243319eb09dd8c282b4deae289a1654138f
Kongxib Vue <kongcvue@gmail.com>
2021-12-15 12:48:01 -0600

    created all admin reducers

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,15 +1,29 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
+import adminSiteReducer from './adminSite.reducer';
+import adminLanguagesReducer from './adminLanguages.reducer';
+import adminCategoriesReducer from './adminCategories.reducer';
+import adminApprovalsReducer from './adminApprovals.reducer';
+import adminEditReducer from './adminEdit.reducer';
+
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
 
+const adminReducer = combineReducers({
+  adminSiteReducer,
+  adminLanguagesReducer,
+  adminCategoriesReducer,
+  adminApprovalsReducer,
+  adminEditReducer
+})
+
 // Lets make a bigger object for our store, with the objects from our reducers.
 // This is what we get when we use 'state' inside of 'mapStateToProps'
 const rootReducer = combineReducers({
-  errors, // contains registrationMessage and loginMessage
+  adminReducer, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
 });
 
