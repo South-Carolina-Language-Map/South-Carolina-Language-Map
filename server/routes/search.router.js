f1c1fe723d05313403569c3478083da8977ce707
Julia Bayless <julia.allistair@gmail.com>
2021-12-15 16:22:10 -0600

    redoing search query

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -6,45 +6,33 @@ const axios = require('axios');
 require('dotenv');
 
 
-async function search(req, res) {
-    //creating variable to hold search queries
-    console.log('=========', req.query)
-    let reducer = req.query;
-
-    let queryString = '';
-
-    //loop through to find if there are multiple from explore view
-    for (let key in reducer) {
-        if (queryString.length === 0) {
-            queryString += '?';
-        } else {
-            //will need to add a search param if there are more than one
-            queryString += '&';
-        }
-        
-        let value = req.query[key]
-        //add on new search param with each loop
-        queryString += `${key}=${value}`;
-        console.log('this is queryString====>', queryString)
-    }
-
- 
-  //GET with axios and api key
-    const response =
-        await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${queryString}.json?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`)
-           .then((response) => {
-            console.log(`SEARCH GET response ->`, response.data);
-            res.send(response.data);
-        })
-        .catch((err) => {
-            console.log(`Error in get ->`, err);
-        });
-}
-
-
- //req.query
+//req.query
 //GET for search function
-router.get('/', search, (req, res) => { });
+router.get('/', (req, res) => {
 
+    console.log('this is req.query ==>', req.query);
+
+
+    searchQueryText = `
+    SELECT * FROM "sites"
+    JOIN "regions" ON "regions".id = "sites".region_id
+    JOIN "languages" ON "languages".id = "sites".language_id
+    JOIN "categories" ON "languages".category_id = "categories".id
+    LEFT JOIN "examples" ON "languages".id = "examples".language_id
+    WHERE $1 ILIKE $2 ORDER BY "name" ASC;
+    `;
+
+    params = [searchQueryText, [`%${req.query.key}%`]];
+
+    pool.query(...params)
+        .then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+
+
+});
 
 module.exports = router;
