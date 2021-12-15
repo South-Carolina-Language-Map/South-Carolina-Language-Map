3618383409099aab3036d68e4f53ea143277b430
Nick Allen <rutherford.t.b@gmail.com>
2021-12-15 13:58:29 -0600

    renamed reducers to 'SET' actions, updated _actions.md

--- src/redux/reducers/adminReducers/adminLanguages.reducer.js
+++ src/redux/reducers/adminReducers/adminLanguages.reducer.js
@@ -1,6 +1,6 @@
 const adminLanguagesReducer = (state = [], action) => {
     switch (action.type) {
-      case 'FETCH_LANGUAGES':
+      case 'SET_LANGUAGES':
         return [...action.payload];
       default:
         return state;
