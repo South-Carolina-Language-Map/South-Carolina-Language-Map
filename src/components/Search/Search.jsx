a575fb13ba03c6b6ef73accb84a770decbf0c7d0
Jack Barrett <jbarrett1347@gmail.com>
2021-12-16 14:05:19 -0600

    Added better styling

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -18,119 +18,124 @@ import SearchIcon from "@mui/icons-material/Search";
 
 function Search() {
   return (
-      <Box
-        sx={{
-          textAlign: "center"
-          // width: 500,
-        }}
-      >
-        {/* <Typography
-          variant="h4"
-          sx={{ backgroundColor: "primary.light", p: 2 }}
-        >
-          Search and Display Sites
-        </Typography> */}
+    <Box
+      sx={{
+        pr: 1,
+        pl: 1,
+        textAlign: "center",
+      }}
+    >
+      <br />
+
+      <FormControl sx={{ width: 2 / 2 }}>
+        <TextField id="search-with-sx" label="Search" variant="standard" />
+
         <br />
 
-        <FormControl sx={{ pr: 1, pl: 1 }}>
-          <TextField id="search-with-sx" label="Search" variant="standard" />
+        <Typography variant="body2" className="textLeft">
+          Search by:
+        </Typography>
 
-          <br />
-
-          <Typography variant="body2" className="textLeft">
-            Search by:
-          </Typography>
-
-          <RadioGroup>
-            <Stack direction="row" spacing={2}>
+        <RadioGroup>
+          {/* <Stack direction="row" > */}
+          <Grid container>
+            <Grid item xs={6}>
               <FormControlLabel value="Site" label="Site" control={<Radio />} />
+            </Grid>
+            <Grid item xs={6}>
               <FormControlLabel
                 value="Language"
                 label="Language"
                 control={<Radio />}
               />
+            </Grid>
+            <Grid item xs={6}>
               <FormControlLabel
                 value="Region"
                 label="Region"
                 control={<Radio />}
               />
+            </Grid>
+            <Grid item xs={6}>
               <FormControlLabel
                 value="Category"
                 label="Category"
                 control={<Radio />}
               />
-            </Stack>
-          </RadioGroup>
-          <br />
-          <Button
-            sx={{
-              color: "primary.dark",
-            }}
-          >
-            Clear Search
-          </Button>
-
-          <br />
-          <Divider orientation="horizontal" />
-          <br />
-        </FormControl>
-
-        <Paper elevation={8} sx={{ textAlign: "center" }}>
-          <Typography variant="h5">Language Catagories</Typography>
-
-          <br />
-
-          <Grid container spacing={2} rowSpacing={4} sx={{ pr: 1, pl: 1 }}>
-            <Grid item xs={4}>
-              <Stack direction="row" spacing={1}>
-                <div className="lang-asian" />
-                <Typography>Asian</Typography>
-              </Stack>
-            </Grid>
-
-            <Grid item xs={4}>
-              <Stack direction="row" spacing={1}>
-                <div className="lang-latino" />
-                <Typography>Latino</Typography>
-              </Stack>
-            </Grid>
-
-            <Grid item xs={4}>
-              <Stack direction="row" spacing={1}>
-                <div className="lang-european" />
-                <Typography>European</Typography>
-              </Stack>
-            </Grid>
-            <Grid item xs={4}>
-              <Stack direction="row" spacing={1}>
-                <div className="lang-middle-east" />
-                <Typography>Middle East</Typography>
-              </Stack>
-            </Grid>
-            <Grid item xs={4}>
-              <Stack direction="row" spacing={1}>
-                <div className="lang-sign-language" />
-                <Typography>Sign Language</Typography>
-              </Stack>
-            </Grid>
-            <Grid item xs={4}>
-              <Stack direction="row">
-                <div className="lang-native-american" />{" "}
-                <Typography>Native American</Typography>
-              </Stack>
-            </Grid>
-            <Grid item xs={4} />
-            <Grid item xs={5}>
-              <Stack direction="row" spacing={1}>
-                <div className="lang-varieties-of-english" />
-                <Typography>
-                  Varieties of <br /> English
-                </Typography>
-              </Stack>
             </Grid>
           </Grid>
-        </Paper>
-      </Box>
+          {/* </Stack> */}
+        </RadioGroup>
+        <br />
+        <Button
+          sx={{
+            color: "primary.dark",
+          }}
+        >
+          Clear Search
+        </Button>
+
+        <br />
+        <Divider orientation="horizontal" />
+        <br />
+      </FormControl>
+
+      <Paper elevation={8} sx={{ textAlign: "center" }}>
+        <Typography variant="h5">Language Catagories</Typography>
+
+        <br />
+
+        <Grid container spacing={2} rowSpacing={4} sx={{ pr: 1, pl: 1 }}>
+          <Grid item xs={4}>
+            <Stack direction="row" spacing={1}>
+              <div className="lang-asian" />
+              <Typography>Asian</Typography>
+            </Stack>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Stack direction="row" spacing={1}>
+              <div className="lang-latino" />
+              <Typography>Latino</Typography>
+            </Stack>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Stack direction="row" spacing={1}>
+              <div className="lang-european" />
+              <Typography>European</Typography>
+            </Stack>
+          </Grid>
+          <Grid item xs={4}>
+            <Stack direction="row" spacing={1}>
+              <div className="lang-middle-east" />
+              <Typography>Middle East</Typography>
+            </Stack>
+          </Grid>
+          <Grid item xs={4}>
+            <Stack direction="row" spacing={1}>
+              <div className="lang-sign-language" />
+              <Typography>Sign Language</Typography>
+            </Stack>
+          </Grid>
+          <Grid item xs={4}>
+            <Stack direction="row">
+              <div className="lang-native-american" />{" "}
+              <Typography>Native American</Typography>
+            </Stack>
+          </Grid>
+          <Grid item xs={4} />
+          <Grid item xs={5}>
+            <Stack direction="row" spacing={1}>
+              <div className="lang-varieties-of-english" />
+              <Typography>
+                Varieties of <br /> English
+              </Typography>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Box>
   );
 }
 
