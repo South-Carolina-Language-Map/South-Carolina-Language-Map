2ebe8c06ddced5adeae4962aa73de4225aec759f
Nick Allen <rutherford.t.b@gmail.com>
2021-12-16 09:57:47 -0600

    fixed multiple reducers

--- src/redux/reducers/adminReducers/adminCategories.reducer.js
+++
@@ -1,9 +0,0 @@
-const adminCategoriesReducer = (state = [], action) => {
-    switch (action.type) {
-      case 'SET_CATEGORIES':
-        return [...action.payload];
-      default:
-        return state;
-    }
-  };
-  export default adminCategoriesReducer;
--- src/redux/reducers/adminReducers/adminLanguages.reducer.js
+++
@@ -1,9 +0,0 @@
-const adminLanguagesReducer = (state = [], action) => {
-    switch (action.type) {
-      case 'SET_LANGUAGES':
-        return [...action.payload];
-      default:
-        return state;
-    }
-  };
-  export default adminLanguagesReducer;
