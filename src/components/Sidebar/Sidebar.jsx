a1d500ada8189c1590372ffd4eedb0ce0d12a7df
Jack Barrett <jbarrett1347@gmail.com>
2021-12-17 11:40:32 -0600

    Revampedheader display and removed unneeded functions

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,13 @@
 import Map from "../Map/Map";
-import { useEffect, useState } from "react";
 import { Box } from "@mui/system";
 import Search from "../Search/Search";
 import NavInfo from "../NavInfo/NavInfo";
-import BottomNav from "../BottomNav/BottomNav";
-import NavExplorer from "../NavExplorer/NavExplorer";
-import { Grid, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
+import BottomNav from "../BottomNav/BottomNav";
+import { Grid, Typography } from "@mui/material";
+import NavExplorer from "../NavExplorer/NavExplorer";
 
 function Sidebar() {
-  //   const [currentView, setCurrentView] = useState(<Search />);
   let currentView;
   let currentViewHeaderText = "Info Page";
   const sideBarView = useSelector((store) => store.sideBarView);
@@ -17,62 +15,45 @@ function Sidebar() {
   switch (sideBarView) {
     case "search":
       currentView = <Search />;
+      currentViewHeaderText = "Search";
       break;
     case "explore":
       currentView = <NavExplorer />;
+      currentViewHeaderText = "Explore";
       break;
     case "info":
       currentView = <NavInfo />;
+      currentViewHeaderText = "About";
       break;
     default:
-        currentView = <p>nick error</p>
-    break;
-  }
-
-  function chooseSearch() {
-    setCurrentView(<Search />);
-    currentViewHeaderText = "Search Page";
-  }
-
-  function chooseInfo() {
-    return (
-      (setCurrentView = <NavInfo />), (currentViewHeaderText = "Info Page")
-    );
-  }
-
-  function chooseExplore() {
-    return (
-      (setCurrentView = <NavExplorer />),
-      (currentViewHeaderText = "Explore Page")
-    );
+      currentView = <p>There was an Error, please try again later</p>;
+      break;
   }
 
   return (
-    <>
-      <Grid container>
-        <Grid item xs={4}>
-          <Box label="componentsContainer">
-            <Box>
-              <Typography
-                variant="h4"
-                sx={{
-                  p: 2,
-                  textAlign: "center",
-                  backgroundColor: "primary.light",
-                }}
-              >
-                {currentViewHeaderText}
-              </Typography>
-            </Box>
-            {currentView}
-            <BottomNav />
+    <Grid container>
+      <Grid item xs={4}>
+        <Box label="componentsContainer">
+          <Box>
+            <Typography
+              variant="h4"
+              sx={{
+                p: 2,
+                textAlign: "center",
+                backgroundColor: "primary.light",
+              }}
+            >
+              {currentViewHeaderText}
+            </Typography>
           </Box>
-        </Grid>
-        <Grid item xs={8}>
-          <Map />
-        </Grid>
+          {currentView}
+          <BottomNav />
+        </Box>
       </Grid>
-    </>
+      <Grid item xs={8}>
+        <Map />
+      </Grid>
+    </Grid>
   );
 }
 
