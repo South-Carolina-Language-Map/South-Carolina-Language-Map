19254d614a9d3eda01002625bc130f1d8c44c063
Nick Allen <rutherford.t.b@gmail.com>
2021-12-17 15:17:57 -0600

    map fits grid

--- src/components/GridView/GridView.jsx
+++ src/components/GridView/GridView.jsx
@@ -1,12 +1,13 @@
 import Map from "../Map/Map.jsx";
 import { Grid } from "@mui/material";
 import Sidebar from "../Sidebar/Sidebar.jsx";
+import './GridView.css';
 
 function GridView() {
   return (
-    <Grid container direction="row-reverse">
-      <Grid item xs={12} sm={8} md={8} lg={8} xl={8}>
-        <Map />
+    <Grid container direction="row-reverse"> 
+      <Grid item xs={12} sm={8} md={8} lg={8} xl={8} >
+        <Map />   
       </Grid>
       <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
         <Sidebar />
