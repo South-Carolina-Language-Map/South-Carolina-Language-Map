2b069bac43b121dd43b26f1e530f41df1f83cd1a
Jack Barrett <jbarrett1347@gmail.com>
2022-01-03 11:01:52 -0600

    Working on adding what Xibz sent me

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -1,15 +1,12 @@
+import "./BottomNav.css";
 import * as React from "react";
 import InfoIcon from "@mui/icons-material/Info";
 import SearchIcon from "@mui/icons-material/Search";
 import ExploreIcon from "@mui/icons-material/Explore";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import { useDispatch, useSelector } from "react-redux";
-
-import "./BottomNav.css";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 
 function BottomNav() {
-
   let value = 0;
   const dispatch = useDispatch();
   const sideBarView = useSelector((store) => store.sideBarView);
@@ -34,10 +31,9 @@ function BottomNav() {
       value={value}
       className="test"
       sx={{
-        // bottom: 0,
-        // position: "fixed",
-        // OR THIS
-        position: "relative",
+        bottom: 0,
+        width: 0.665 / 2,
+        position: 'fixed'
       }}
     >
       <BottomNavigationAction
