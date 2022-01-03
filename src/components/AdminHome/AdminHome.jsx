1cdd1b27f6e391b95c5a9d16b654a935fc07b7b6
Julia Bayless <julia.allistair@gmail.com>
2022-01-03 13:21:09 -0600

    set up for new nav bar

--- src/components/AdminHome/AdminHome.jsx
+++ src/components/AdminHome/AdminHome.jsx
@@ -30,88 +30,10 @@ function AdminHome() {
   };
   
   return (
+    <>
     <p>ADMIN SITES VIEW</p>
-    // <>
-    // <Grid container spacing={3}>
-    //   <Grid item xs>
-    //     <TextField
-    //         required
-    //         id="filled-required"
-    //         label="Required"
-    //         variant="standard"
-    //         helperText="ex. Raleigh"
-    //       />
-    //   </Grid>
-    //   <Grid item xs>
-    //     <TextField
-    //         required
-    //         id="filled-required"
-    //         label="Required"
-    //         variant="standard"
-    //         helperText="Address"
-    //       />
-    //   </Grid>
-    //     <Grid item xs>
-    //       <AutoCompleteLanguage/>
-    //     </Grid>
-    //   <Grid item xs>
-    //     <Button variant="contained" endIcon={<PublishIcon/>}>
-    //       Submit
-    //     </Button>
-    //   </Grid>
-    // </Grid>
-
-    // <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-    //   <TableContainer sx={{ maxHeight: 440 }}>
-    //     <Table stickyHeader aria-label="sticky table">
-    //       <TableHead>
-    //         <TableRow>
-    //           {columns.map((column) => (
-    //             <TableCell
-    //               key={column.id}
-    //               align={column.align}
-    //               style={{ minWidth: column.minWidth }}
-    //             >
-    //               {column.label}
-    //             </TableCell>
-    //           ))}
-    //         </TableRow>
-    //       </TableHead>
-    //       <TableBody>
-    //         {rows
-    //           .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-    //           .map((row) => {
-    //             return (
-    //               <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-    //                 {columns.map((column) => {
-    //                   const value = row[column.id];
-    //                   return (
-    //                     <TableCell key={column.id} align={column.align}>
-    //                       {column.format && typeof value === 'number'
-    //                         ? column.format(value)
-    //                         : value}
-    //                     </TableCell>
-    //                   );
-    //                 })}
-    //               </TableRow>
-    //             );
-    //           })}
-    //       </TableBody>
-    //     </Table>
-    //   </TableContainer>
-    //   <TablePagination
-    //     rowsPerPageOptions={[10, 25, 100]}
-    //     component="div"
-    //     count={rows.length}
-    //     rowsPerPage={rowsPerPage}
-    //     page={page}
-    //     onPageChange={handleChangePage}
-    //     onRowsPerPageChange={handleChangeRowsPerPage}
-    //   />
-    // </Paper>
     
-    // </>
-
+    </>
   );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+//components
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
-import { useSelector } from 'react-redux';
+
+//mui styles
+import { 
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
+} from '@mui/material';
+
 
 function Nav() {
   const user = useSelector((store) => store.user);
@@ -14,7 +27,7 @@ function Nav() {
       </Link>
       <div>
         {/* If no user is logged in, show these links */}
-        {user.id === null &&
+        {user.id === null  || user.clearance_level === 0 &&
           // If there's no user, show login/registration links
           <Link className="navLink" to="/login">
             Login / Register
@@ -22,7 +35,7 @@ function Nav() {
         }
 
         {/* If a user is logged in, show these links */}
-        {user.id && (
+        {user.clearance_level >= 1 && (
           <>
             <Link className="navLink" to="/admin/site">
               Site
