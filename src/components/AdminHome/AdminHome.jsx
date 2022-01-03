641ba50c5c211749b4d7bd1a79b061999bf73517
Jack Barrett <jbarrett1347@gmail.com>
2022-01-03 14:17:30 -0600

    Working on the table view(s) in admin

--- src/components/AdminHome/AdminHome.jsx
+++ src/components/AdminHome/AdminHome.jsx
@@ -20,6 +20,10 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
+
+import AdminTable from "../AdminTable/AdminTable";
+
+
 function AdminHome() {
   const dispatch = useDispatch();
   const sites = useSelector((store) => store.viewReducer.sitesReducer);
@@ -71,53 +75,9 @@ function AdminHome() {
           </Button>
         </Grid>
       </Grid>
-      
-    {/* <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-                <TableCell
-                  align="left"
-                  style={{ minWidth: 170}}
-                >
-                  SiteName
-                </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {sites.map((site) => {
-                      const value = row[site.id];
-                      return (
-                        <TableCell key={site.id} align={site.align}>
-                          {site.format && typeof value === 'number'
-                            ? site.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper> */}
-    </>
+
+      <AdminTable />
+      </>
   );
 }
 
---
+++ src/components/AdminTable/AdminTable.jsx
@@ -0,0 +1,55 @@
+function AdminTable() {
+  return (
+    <>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left" style={{ minWidth: 170 }}>
+                  SiteName
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.code}
+                    >
+                      {sites.map((site) => {
+                        const value = row[site.id];
+                        return (
+                          <TableCell key={site.id} align={site.align}>
+                            {site.format && typeof value === "number"
+                              ? site.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </>
+  );
+}
+
+export default AdminTable;
