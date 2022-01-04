ca063e8230973fc79f88ebdd04eb6acd254caf9a
Jack Barrett <jbarrett1347@gmail.com>
2022-01-04 15:00:05 -0600

    Removed an unneeded comment

--- src/components/AdminLanguage/AdminLanguage.jsx
+++ src/components/AdminLanguage/AdminLanguage.jsx
@@ -109,61 +109,6 @@ function AdminLanguage() {
       </Grid>
       <Grid item xs={1} />
     </Grid>
-    // <Grid container sx={{ pt: 3 }}>
-    //   <Grid item xs={1} />
-    //   <Grid item xs={10}>
-    //     <Table stickyHeader aria-label="sticky table">
-    //       <TableHead>
-    //         <TableRow>
-    //           <TableCell align="center">Name</TableCell>
-    //           <TableCell align="center">Glottocode</TableCell>
-    //           <TableCell align="center">Global Speakers</TableCell>
-    //           <TableCell align="center">SC Speakers</TableCell>
-    //           <TableCell align="center">Endonym</TableCell>
-    //           <TableCell align="center">Edit/Delete</TableCell>
-    //         </TableRow>
-    //       </TableHead>
-    //       <TableBody>
-    //         {languages.map((row) => (
-    //           <TableRow>
-    //             <TableCell component="th" scope="row" align="center">
-    //               {row.language}
-    //             </TableCell>
-    //             <TableCell component="th" scope="row" align="center">
-    //               {row.glottocode}
-    //             </TableCell>
-    //             <TableCell component="th" scope="row" align="center">
-    //               {row.global_speakers}
-    //             </TableCell>
-    //             <TableCell component="th" scope="row" align="center">
-    //               {row.sc_speakers}
-    //             </TableCell>
-    //             <TableCell component="th" scope="row" align="center">
-    //               {row.endonym}
-    //             </TableCell>
-    //             <TableCell align="center">
-    //               <Button
-    //                 sx={{ mr: 1, mb: 1 }}
-    //                 variant="contained"
-    //                 onClick={handleEdit}
-    //               >
-    //                 Edit
-    //               </Button>
-    //               <Button
-    //                 sx={{ mb: 1 }}
-    //                 variant="contained"
-    //                 onClick={handleDelete}
-    //               >
-    //                 Delete
-    //               </Button>
-    //             </TableCell>
-    //           </TableRow>
-    //         ))}
-    //       </TableBody>
-    //     </Table>
-    //   </Grid>
-    //   <Grid item xs={1} />
-    // </Grid>
   );
 }
 
