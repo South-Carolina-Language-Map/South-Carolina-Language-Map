4ccdadc5c64afda04e2c70889fdf4db2ee3b771f
Nick Allen <rutherford.t.b@gmail.com>
2022-01-05 09:29:01 -0600

    add console.log, jumping to work elsewhere

--- src/components/NavExplorer/NavExplorer.jsx
+++ src/components/NavExplorer/NavExplorer.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
 
 import { useDispatch, useSelector } from 'react-redux';
 import NavExploreItem from '../NavExploreItem/NavExploreItem';
@@ -12,20 +13,23 @@ function NavExplorer() {
   const list = useSelector(store => store.viewReducer.listReducer);
   const dispatch = useDispatch();
   const [activeKey, setActiveKey] = useState('name');
+  const [prevState, setPrevState] = useState([]);
 
   useEffect(() => {
     dispatch({type: 'SET_LIST_DEFAULT'});
   },[]);
 
-  console.log(list);
+  console.log('prevState:' prevState)
 
   return (
     <>
+      {prevState.length > 0 && <Button>Back</Button>}
       <Grid container spacing={2}>
         {list && list.map((listObj, i) => {
           return <NavExploreItem key={i} listObj={listObj} 
           activeKey={activeKey}
-          setActiveKey={setActiveKey}/>
+          setActiveKey={setActiveKey}
+          setPrevState={setPrevState}/>
         })}
       </Grid>
     </>
