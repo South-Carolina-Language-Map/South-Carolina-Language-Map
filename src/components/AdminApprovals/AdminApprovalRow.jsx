cd2ee394ed0ddd421dd49134d8a3faced36cec70
Julia Bayless <julia.allistair@gmail.com>
2022-01-05 11:17:27 -0600

    component successfully intergrated, need to connect delete pathway

--- src/components/AdminApprovals/AdminApprovalRow.jsx
+++ src/components/AdminApprovals/AdminApprovalRow.jsx
@@ -1,7 +1,49 @@
+import {
+    Grid,
+    Table,
+    Button,
+    TableRow,
+    TableBody,
+    TableCell,
+    TableHead,
+} from "@mui/material";
+
+export default function AdminApprovalRow({ user }) {
+
+    //edit clearance_level from 0 -> 1 creating admin clearance
+    const handleApproval = () => {
+        console.log("Edit");
+    };
+
+    //delete from user table
+    const handleRejection = () => {
+        console.log("Delete");
+    };
 
 
-export default function AdminApprovalRow() {
-
-
-
+    return (
+        <TableRow>
+            <TableCell component="th" scope="row" align="center">
+                {user.fullName}
+            </TableCell>
+            <TableCell component="th" scope="row" align="center">
+                {user.username}
+            </TableCell>
+            <TableCell component="th" scope="row" align="center">
+                {user.email}
+            </TableCell>
+            <TableCell align="center">
+                <Button
+                    sx={{ mr: 1 }}
+                    variant="contained"
+                    onClick={handleApproval}
+                >
+                    Approve
+                </Button>
+                <Button variant="contained" onClick={handleRejection}>
+                    Decline
+                </Button>
+            </TableCell>
+        </TableRow>
+    )
 } //end AdminApprovalRow
