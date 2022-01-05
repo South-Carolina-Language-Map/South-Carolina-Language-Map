2abd715ca396b3e1ca926073d2201889e1849a24
Julia Bayless <julia.allistair@gmail.com>
2022-01-05 09:47:50 -0600

    security set up for category router

--- server/routes/categories.router.js
+++ server/routes/categories.router.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 //GET all from category list
 router.get('/', (req, res) => {
+  console.log(req.user)
   pool.query(`SELECT * FROM "categories";`)
     .then((response) => {
       res.send(response.rows);
@@ -19,21 +20,30 @@ router.get('/', (req, res) => {
 
 //POST - add new category
 router.post('/', (req, res) => {
+  console.log(req.user)
   const name = req.body.name;
-  const queryText = `
+
+  //security
+  const clearanceLevel = req.user.clearance_level
+
+  if (clearanceLevel >= 1) {
+    const queryText = `
   INSERT INTO "categories" (name)
   VALUES ($1);
   `;
 
-  pool.query(queryText, [name])
-    .then(() => {
-      res.sendStatus(201);
-      console.log('POST Categories success');
-    })
-    .catch((err) => {
-      sendStatus(500);
-      console.log('ERROR in Categories POST', err);
-    });
+    pool.query(queryText, [name])
+      .then(() => {
+        res.sendStatus(201);
+        console.log('POST Categories success');
+      })
+      .catch((err) => {
+        sendStatus(500);
+        console.log('ERROR in Categories POST', err);
+      });
+  } else {
+    res.sendStatus(403)
+  } //end if conditional
 });
 
 
@@ -42,40 +52,57 @@ router.put('/:id', (req, res) => {
   const id = req.params.id;
   const name = req.body.name;
 
-  const queryText = `
+  //security - for admin use only
+  const clearanceLevel = req.user.clearance_level
+
+  if (clearanceLevel >= 1) {
+
+    const queryText = `
   UPDATE "categories"
   SET "name" = $1
   WHERE "id" = $2
   ;`
 
-  pool.query(queryText, [name, id])
-    .then(() => {
-      res.sendStatus(200);
-      console.log('PUT Categories success');
-    })
-    .catch((err) => {
-      res.sendStatus(500);
-      console.log('ERROR in Categories PUT', err);
-    })
+    pool.query(queryText, [name, id])
+      .then(() => {
+        res.sendStatus(200);
+        console.log('PUT Categories success');
+      })
+      .catch((err) => {
+        res.sendStatus(500);
+        console.log('ERROR in Categories PUT', err);
+      })
+  } else {
+    res.sendStatus(403)
+  } //end if conditional
 });
 
 
 //DELETE a specific category
 router.delete('/:id', (req, res) => {
   const categoryID = req.params.id;
-  const queryText = `
+
+  //security - for admin use only
+  const clearanceLevel = req.user.clearance_level
+
+  if (clearanceLevel >= 1) {
+
+    const queryText = `
   DELETE FROM "categories" WHERE "id" = $1
   ;`
 
-  pool.query(queryText, [categoryID])
-    .then(() => {
-      console.log('DELETE Categories success')
-      res.sendStatus(200)
-    })
-    .catch((err) => {
-      console.log('ERROR in Categories DELETE', err);
-      res.sendStatus(500);
-    })
+    pool.query(queryText, [categoryID])
+      .then(() => {
+        console.log('DELETE Categories success')
+        res.sendStatus(200)
+      })
+      .catch((err) => {
+        console.log('ERROR in Categories DELETE', err);
+        res.sendStatus(500);
+      })
+  } else {
+    res.sendStatus(403)
+  } //end if conditional
 });
 
 
