2f8b8e791a25d2671d2c33b01e7181fd8dfdb0bb
Julia Bayless <julia.allistair@gmail.com>
2022-01-06 14:22:57 -0600

    toggle edit text field built into category row, need to dispatch result

--- src/components/AdminCategory/AdminCategoryRow.jsx
+++ src/components/AdminCategory/AdminCategoryRow.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 
 // MUI Imports
@@ -6,6 +7,7 @@ import {
     Button,
     TableRow,
     TableCell,
+    TextField
 } from "@mui/material";
 
 export default function AdminCategoryRow({ row }) {
@@ -13,12 +15,21 @@ export default function AdminCategoryRow({ row }) {
     //hooks
     const dispatch = useDispatch();
 
+    //useStates
+    const [handleEditMode, setHandleEditMode] = useState(false);
+
+    const CallEditMode = () => {
+        console.log("CallEditMode", row.id);
+        setHandleEditMode(true)
+    }
+
+
     // function to edit this category
     const handleEdit = () => {
-        console.log("Edit", row.id);
+        console.log("HandleEdit", row.id);
     };
 
-    
+
     //function to delete this row assocaited with ID
     const handleDelete = () => {
         console.log("Delete", row.id);
@@ -30,24 +41,46 @@ export default function AdminCategoryRow({ row }) {
 
     return (
         <>
-            <TableRow>
-                <TableCell component="th" scope="row" align="center">
-                    {row.name}
-                </TableCell>
-                <TableCell align="center">
-                    <Button
-                        sx={{ mr: 1 }}
-                        variant="contained"
-                        onClick={handleEdit}
-                    >
-                        Edit
-                    </Button>
-                    <Button variant="contained" onClick={handleDelete}>
-                        Delete
-                    </Button>
-                </TableCell>
-            </TableRow>
-
+            {!handleEditMode ?
+                <TableRow>
+                    <TableCell component="th" scope="row" align="center">
+                        {row.name}
+                    </TableCell>
+                    <TableCell align="center">
+                        <Button
+                            sx={{ mr: 1 }}
+                            variant="contained"
+                            onClick={CallEditMode}
+                        >
+                            Edit
+                        </Button>
+                        <Button variant="contained" onClick={handleDelete}>
+                            Delete
+                        </Button>
+                    </TableCell>
+                </TableRow>
+                :
+                <TableRow>
+                    <TableCell align="center">
+                        <TextField
+                            id="standard-basic"
+                            label="Standard"
+                            variant="standard"
+                            value={row.name} />
+                    </TableCell>
+                    <TableCell align="center">
+                        <Button
+                            sx={{ mr: 1 }}
+                            variant="contained"
+                            onClick={handleEdit}
+                        >
+                            Submit
+                        </Button>
+                        <Button variant="contained" onClick={() =>setHandleEditMode(false)}>
+                            Cancel
+                        </Button>
+                    </TableCell>
+                </TableRow> }
         </>
     )
 }//end adminCategoryRow
