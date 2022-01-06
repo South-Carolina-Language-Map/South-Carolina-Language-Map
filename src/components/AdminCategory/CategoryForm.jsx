e3f7f5fbbd495957ea425bd736eb394428f198e0
Kongxib Vue <kongcvue@gmail.com>
2022-01-06 11:24:14 -0600

    added typography, needs formatting for grid layout

--- src/components/AdminCategory/CategoryForm.jsx
+++ src/components/AdminCategory/CategoryForm.jsx
@@ -3,23 +3,26 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Grid, TextField, Button } from "@mui/material";
+import { Grid, TextField, Button, Typography } from "@mui/material";
 import PublishIcon from "@mui/icons-material/Publish";
 
 function CategoryForm() {
   const dispatch = useDispatch();
-  //initialize the value of the input 
-  const [newCategory, setCategory] = useState('');
+  //initialize the value of the input
+  const [newCategory, setCategory] = useState("");
   console.log("newCategory", newCategory);
   return (
-    <form onSubmit={()=> dispatch({type: "ADD_CATEGORY", payload: newCategory})}>
+    <form
+      onSubmit={() => dispatch({ type: "ADD_CATEGORY", payload: newCategory })}
+    >
+      <Typography>Add a new category!</Typography>
       <TextField
         required
         id="filled-required"
         label="Category Name"
         variant="standard"
         helperText="ex. Native American"
-        onChange={(event) => setCategory({name: event.target.value})}
+        onChange={(event) => setCategory({ name: event.target.value })}
       />
       <Button type="submit" variant="contained" endIcon={<PublishIcon />}>
         Submit
