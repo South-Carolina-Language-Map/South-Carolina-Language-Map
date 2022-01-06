6d99d4bf93a3f377072efa976c1370b2159c51ba
Jack Barrett <jbarrett1347@gmail.com>
2022-01-06 12:09:40 -0600

    Adding styling to explore

--- src/components/NavExplorer/NavExplorer.jsx
+++ src/components/NavExplorer/NavExplorer.jsx
@@ -1,40 +1,47 @@
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import { Button } from '@mui/material';
+// MUI Imports
+import { Grid, Button } from "@mui/material";
 
-import { useDispatch, useSelector } from 'react-redux';
-import NavExploreItem from '../NavExploreItem/NavExploreItem';
-import { useEffect, useState } from 'react';
+// React Imports
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+// Local Imports
+import NavExploreItem from "../NavExploreItem/NavExploreItem";
 
 function NavExplorer() {
-  const list = useSelector(store => store.viewReducer.listReducer);
-  const listType = useSelector(store => store.viewReducer.listTypeReducer);
+  const list = useSelector((store) => store.viewReducer.listReducer);
+  const listType = useSelector((store) => store.viewReducer.listTypeReducer);
   const dispatch = useDispatch();
-  const [activeKey, setActiveKey] = useState('name');
-  const [prevState, setPrevState] = useState([{
-    list: list,
-    listType: listType || 'DEFAULT',
-  }]);
+  const [activeKey, setActiveKey] = useState("name");
+  const [prevState, setPrevState] = useState([
+    {
+      list: list,
+      listType: listType || "DEFAULT",
+    },
+  ]);
 
   useEffect(() => {
-    dispatch({type: 'SET_LIST_DEFAULT'});
-  },[]);
+    dispatch({ type: "SET_LIST_DEFAULT" });
+  }, []);
 
-  console.log('prevState:', prevState)
+  console.log("prevState:", prevState);
 
   return (
     <>
       {/* {prevState[0].listType !== 'DEFAULT' && <Button>Back</Button>} */}
       <Grid container spacing={2}>
-        {list && list.map((listObj, i) => {
-          return <NavExploreItem key={i} listObj={listObj} 
-          activeKey={activeKey}
-          setActiveKey={setActiveKey}
-          setPrevState={setPrevState}/>
-        })}
+        {list &&
+          list.map((listObj, i) => {
+            return (
+              <NavExploreItem
+                key={i}
+                listObj={listObj}
+                activeKey={activeKey}
+                setActiveKey={setActiveKey}
+                setPrevState={setPrevState}
+              />
+            );
+          })}
       </Grid>
     </>
   );
