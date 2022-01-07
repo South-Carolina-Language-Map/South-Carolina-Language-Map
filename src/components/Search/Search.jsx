58b4071e2cf7479f8a368472ea849fb7fce20651
Nick Allen <rutherford.t.b@gmail.com>
2022-01-07 10:10:04 -0600

    refactore into Legend Component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -24,6 +24,7 @@ import TabPanel from "@mui/lab/TabPanel";
 import TabContext from "@mui/lab/TabContext";
 import SearchIcon from "@mui/icons-material/Search";
 import NavLang from "../NavLang/NavLang";
+import Legend from "../Legend/Legend.jsx";
 import { display } from "@mui/system";
 
 function Search() {
@@ -189,61 +190,7 @@ function Search() {
           displayLangInfo ?
             <NavLang site={sites[0]}></NavLang>
             :
-            <Paper elevation={8} sx={{ textAlign: "center", height: 2 / 2 }}>
-              <Typography variant="h5">Language Catagories</Typography>
-
-              <br />
-
-              <Grid container spacing={2} rowSpacing={6} sx={{ pr: 1, pl: 1 }}>
-                <Grid item xs={4}>
-                  <Stack direction="row" spacing={1}>
-                    <div className="lang-asian" />
-                    <Typography>Asian</Typography>
-                  </Stack>
-                </Grid>
-
-                <Grid item xs={4}>
-                  <Stack direction="row" spacing={1}>
-                    <div className="lang-latino" />
-                    <Typography>Latino</Typography>
-                  </Stack>
-                </Grid>
-
-                <Grid item xs={4}>
-                  <Stack direction="row" spacing={1}>
-                    <div className="lang-european" />
-                    <Typography>European</Typography>
-                  </Stack>
-                </Grid>
-                <Grid item xs={4}>
-                  <Stack direction="row" spacing={1}>
-                    <div className="lang-middle-east" />
-                    <Typography>Middle East</Typography>
-                  </Stack>
-                </Grid>
-                <Grid item xs={4}>
-                  <Stack direction="row" spacing={1}>
-                    <div className="lang-sign-language" />
-                    <Typography>Sign Language</Typography>
-                  </Stack>
-                </Grid>
-                <Grid item xs={4}>
-                  <Stack direction="row">
-                    <div className="lang-native-american" />{" "}
-                    <Typography>Native American</Typography>
-                  </Stack>
-                </Grid>
-                <Grid item xs={4} />
-                <Grid item xs={4}>
-                  <Stack direction="row" spacing={1}>
-                    <div className="lang-varieties-of-english" />
-                    <Typography>
-                      Varieties of <br /> English
-                    </Typography>
-                  </Stack>
-                </Grid>
-              </Grid>
-            </Paper>
+            <Legend></Legend>
           }
 
         </Grid>
