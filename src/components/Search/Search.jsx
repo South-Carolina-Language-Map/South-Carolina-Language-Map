6341eab3a586a2da27819330cdb014a175a19773
Nick Allen <rutherford.t.b@gmail.com>
2022-01-07 10:01:34 -0600

    fixes crash when sites.length is 0

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -36,8 +36,9 @@ function Search() {
 
   // Check if all visible sites are the same language
   if (sites.length === 1
-    || sites.filter(site =>
-      sites[0].language === site.language).length === sites.length
+    || (sites.length > 0 && 
+      sites.filter(site =>
+      sites[0].language === site.language).length === sites.length)
   ) {
     displayLangInfo = true;
   }
@@ -182,62 +183,66 @@ function Search() {
         </Grid>
 
         <Grid item xs={12}>
-          {displayLangInfo && <NavLang site={sites[0]}></NavLang>}
-          <Paper elevation={8} sx={{ textAlign: "center", height: 2 / 2 }}>
-            <Typography variant="h5">Language Catagories</Typography>
+          {displayLangInfo ?
+            <NavLang site={sites[0]}></NavLang>
+            :
+            <Paper elevation={8} sx={{ textAlign: "center", height: 2 / 2 }}>
+              <Typography variant="h5">Language Catagories</Typography>
 
-            <br />
+              <br />
 
-            <Grid container spacing={2} rowSpacing={6} sx={{ pr: 1, pl: 1 }}>
-              <Grid item xs={4}>
-                <Stack direction="row" spacing={1}>
-                  <div className="lang-asian" />
-                  <Typography>Asian</Typography>
-                </Stack>
-              </Grid>
+              <Grid container spacing={2} rowSpacing={6} sx={{ pr: 1, pl: 1 }}>
+                <Grid item xs={4}>
+                  <Stack direction="row" spacing={1}>
+                    <div className="lang-asian" />
+                    <Typography>Asian</Typography>
+                  </Stack>
+                </Grid>
 
-              <Grid item xs={4}>
-                <Stack direction="row" spacing={1}>
-                  <div className="lang-latino" />
-                  <Typography>Latino</Typography>
-                </Stack>
-              </Grid>
+                <Grid item xs={4}>
+                  <Stack direction="row" spacing={1}>
+                    <div className="lang-latino" />
+                    <Typography>Latino</Typography>
+                  </Stack>
+                </Grid>
 
-              <Grid item xs={4}>
-                <Stack direction="row" spacing={1}>
-                  <div className="lang-european" />
-                  <Typography>European</Typography>
-                </Stack>
+                <Grid item xs={4}>
+                  <Stack direction="row" spacing={1}>
+                    <div className="lang-european" />
+                    <Typography>European</Typography>
+                  </Stack>
+                </Grid>
+                <Grid item xs={4}>
+                  <Stack direction="row" spacing={1}>
+                    <div className="lang-middle-east" />
+                    <Typography>Middle East</Typography>
+                  </Stack>
+                </Grid>
+                <Grid item xs={4}>
+                  <Stack direction="row" spacing={1}>
+                    <div className="lang-sign-language" />
+                    <Typography>Sign Language</Typography>
+                  </Stack>
+                </Grid>
+                <Grid item xs={4}>
+                  <Stack direction="row">
+                    <div className="lang-native-american" />{" "}
+                    <Typography>Native American</Typography>
+                  </Stack>
+                </Grid>
+                <Grid item xs={4} />
+                <Grid item xs={4}>
+                  <Stack direction="row" spacing={1}>
+                    <div className="lang-varieties-of-english" />
+                    <Typography>
+                      Varieties of <br /> English
+                    </Typography>
+                  </Stack>
+                </Grid>
               </Grid>
-              <Grid item xs={4}>
-                <Stack direction="row" spacing={1}>
-                  <div className="lang-middle-east" />
-                  <Typography>Middle East</Typography>
-                </Stack>
-              </Grid>
-              <Grid item xs={4}>
-                <Stack direction="row" spacing={1}>
-                  <div className="lang-sign-language" />
-                  <Typography>Sign Language</Typography>
-                </Stack>
-              </Grid>
-              <Grid item xs={4}>
-                <Stack direction="row">
-                  <div className="lang-native-american" />{" "}
-                  <Typography>Native American</Typography>
-                </Stack>
-              </Grid>
-              <Grid item xs={4} />
-              <Grid item xs={4}>
-                <Stack direction="row" spacing={1}>
-                  <div className="lang-varieties-of-english" />
-                  <Typography>
-                    Varieties of <br /> English
-                  </Typography>
-                </Stack>
-              </Grid>
-            </Grid>
-          </Paper>
+            </Paper>
+          }
+
         </Grid>
       </Grid>
     </Box>
